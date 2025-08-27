78c25132bd06f57b7ea4eb459558a9eb267c0cc0
AlexDewd <alexanderschei@proton.me>
2025-08-27 13:28:18 +0200

    Update differentiate.py
    Updating differentiate.py with solution

--- projects/differentiate.py
+++ projects/differentiate.py
@@ -2,10 +2,19 @@ import numpy as np
 
 
 def differentiate(u: np.ndarray, dt: float) -> np.ndarray:
-    raise NotImplementedError
+    out = np.zeros_like(u)
+    out[0] = 1
+    C = (1 - dt)/(1 + dt)
+    for n in range(0, len(out)-1):
+        out[n+1] = C* (out[n] if n>0 else out[0]) 
+    return out
 
 def differentiate_vector(u: np.ndarray, dt: float) -> np.ndarray:
-    raise NotImplementedError
+    out = np.zeros_like(u)
+    out[0] = 1
+    out[1:] = (1 - dt)/(1 + dt)
+    out[:] = np.cumprod(out)
+    return out
 
 def test_differentiate():
     t = np.linspace(0, 1, 10)
@@ -17,4 +26,3 @@ def test_differentiate():
 
 if __name__ == '__main__':
     test_differentiate()
-    
